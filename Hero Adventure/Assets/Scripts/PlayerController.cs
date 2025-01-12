8d4bb0fada70d2559c8b246a14d509fe80bc9fc0
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2025-01-12 14:55:20 +0700

    player sprites and animating the player

--- Hero Adventure/Assets/Scripts/PlayerController.cs
+++ Hero Adventure/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,16 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControl;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    private SpriteRenderer mySpriteRender;
 
     private void Awake()
     {
         playerControl = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -28,16 +33,35 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControl.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.deltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
     }
 }
