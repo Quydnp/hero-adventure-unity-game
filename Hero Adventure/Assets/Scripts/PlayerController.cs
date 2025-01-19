2b347eecc8ba0ad221a135c5fd08111622be515c
Quydnp <quyduong0304@gmail.com>
2025-01-19 20:42:03 +0700

    add sword + attacking enemies + destroy enemies

--- Hero Adventure/Assets/Scripts/PlayerController.cs
+++ Hero Adventure/Assets/Scripts/PlayerController.cs
@@ -1,68 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 4f;
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+
+    [SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
-
-    private void Awake()
-    {
+    private SpriteRenderer mySpriteRender;
+    
+    private bool facingLeft = false;
+    
+    private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         playerControls.Enable();
     }
 
-    private void Update()
-    {
+    private void Update() {
         PlayerInput();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         AdjustPlayerFacingDirection();
         Move();
     }
-    void Start()
-    {
-        
-    }
 
-    private void PlayerInput()
-    {
+    private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
 
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    private void Move() {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection()
-    {
+    private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x)
-        {
-            mySpriteRenderer.flipX = true;
-        }
-        else
-        {
-            mySpriteRenderer.flipX = false;
+        if (mousePos.x < playerScreenPoint.x) {
+            mySpriteRender.flipX = true;
+            FacingLeft = true;
+        } else {
+            mySpriteRender.flipX = false;
+            FacingLeft = false;
         }
     }
 }
