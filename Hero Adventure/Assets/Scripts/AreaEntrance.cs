158b83716a591b3ffb3ba205549ceb525fa3e14c
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2025-01-26 15:12:08 +0700

    add second scene and move next scene

---
+++ Hero Adventure/Assets/Scripts/AreaEntrance.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AreaEntrance : MonoBehaviour
+{
+    [SerializeField] private string transitionName;
+    private void Start()
+    {
+        if (transitionName == SceneManagement.Instance.SceneTransitionName)
+        {
+            PlayerController.Instance.transform.position = this.transform.position;
+        }
+    }
+}
