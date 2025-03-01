2853ea7166047fb337db1165713409ac48b6a911
Huu Tan <tanthde170504@fpt.edu.vn>
2025-03-01 18:24:14 +0700

    Fix Player's Projectile

--- Hero Adventure/Assets/Scripts/Projectile.cs
+++ Hero Adventure/Assets/Scripts/Projectile.cs
@@ -29,23 +29,30 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
-        Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
-        PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
+        if (other.isTrigger) return;
 
-        if (!other.isTrigger && (enemyHealth || indestructible || player))
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+        Indestructible indestructible = other.GetComponent<Indestructible>();
+
+        // Player's projectile
+        if (enemyHealth != null && !isEnemyProjectile)
         {
-            if ((player && isEnemyProjectile) || (enemyHealth && !isEnemyProjectile))
-            {
-                player?.TakeDamage(1, transform);
-                Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
-                Destroy(gameObject);
-            }
-            else if (!other.isTrigger && indestructible)
-            {
-                Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
-                Destroy(gameObject);
-            }
+            enemyHealth.TakeDamage(1);
+            Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        // Enemy's projectile
+        else if (player != null && isEnemyProjectile)
+        {
+            player.TakeDamage(1, transform);
+            Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else if (indestructible != null)
+        {
+            Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
         }
     }
 
