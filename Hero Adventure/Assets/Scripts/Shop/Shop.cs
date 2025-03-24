2bb6bafa47808d3cf0c53f9bb727012366371683
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2025-03-24 13:37:43 +0700

    add upgrade to knight player

--- Hero Adventure/Assets/Scripts/Shop/Shop.cs
+++ Hero Adventure/Assets/Scripts/Shop/Shop.cs
@@ -1,11 +1,16 @@
+﻿using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Shop : MonoBehaviour
 {
     private Sprite _sprite = null;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject _currentPlayer; // Player hiện tại (đã được gán trong Inspector)
+
+    public RuntimeAnimatorController newAnimatorController;
+
     public void BuyDamage()
     {
         Debug.Log("Click BuyDamage");
@@ -54,5 +59,92 @@ public class Shop : MonoBehaviour
         {
             Debug.Log("Not enough gold");
         }
+    }
+
+    public void setPlayerOldPrefab(GameObject oldPlayer)
+    {
+        _currentPlayer = oldPlayer;
+    }
+
+    public void SetGameAnimator(RuntimeAnimatorController animatorController)
+    {
+        newAnimatorController = animatorController;
+    }
+
+    public void BuyKnightPlayer()
+    {
+        if (EconomyManager.Instance.CurrentGold >= 1)
+        {
+            if (PlayerController.Instance.IsKnightPlayer)
+            {
+                Debug.Log("Already Knight Player");
+                return;
+            }
+            Animator animator = _currentPlayer.GetComponent<Animator>();
+            animator.runtimeAnimatorController = newAnimatorController;
+
+            PlayerController.Instance.setKnightProp(8f, 4f);
+            PlayerController.Instance.IsKnightPlayer = true;
+            EconomyManager.Instance.MinusCurrentGold(5);
+        }
+        else
+        {
+            Debug.Log("Not enough gold");
+        }
+
+        //if (EconomyManager.Instance.CurrentGold >= 0)
+        //{
+        //    Debug.Log("Click Buy Player");
+
+        //    if (_currentPlayer != null)
+        //    {
+
+        //        _currentPlayer.SetActive(false);
+
+        //    }
+
+        //    if (_newPlayer != null)
+        //    {
+
+
+        //        Debug.Log("new player is not null");
+
+
+
+
+        //        Vector3 oldPosition = _currentPlayer.transform.position;
+        //        Quaternion oldRotation = _currentPlayer.transform.rotation;
+
+
+
+        //        // Tạo Player mới từ Prefab
+        //        GameObject newPlayer = Instantiate(_newPlayer, oldPosition, oldRotation);
+        //        Debug.Log("Tên của new Player: " +  newPlayer.name);
+        //        // Cập nhật Player hiện tại
+        //        oldWeapon.SetParent(newPlayer.transform);
+        //        PlayerHealth playerHealth = newPlayer.GetComponent<PlayerHealth>();
+        //        if (playerHealth != null)
+        //        {
+        //            // Gán gameOverPanel từ UI chính vào Player mới
+        //            playerHealth.SetGameOverPanel(_gameOverPanel);
+        //            Debug.Log("GameOverPanel đã được gán cho Player mới!");
+        //        }
+        //        _currentPlayer = newPlayer;
+
+        //        Debug.Log("Tên của Player hiện tại: " + _currentPlayer.name);
+
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("new player is null");
+        //    }
+
+
+
+        //}
+        //else
+        //{
+        //    Debug.Log("Not enough gold");
+        //}
     }
 }
